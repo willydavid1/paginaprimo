aecc0a356abf9a1eb4cf9fd91257a700d9190a89
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-02 10:41:03 -0500

    cambios realizados por el primo

--- js/animaciones.js
+++ js/animaciones.js
@@ -16,18 +16,6 @@ const callback = (entries, observer) => {
       entry.target.classList.add("animated", "fadeIn");
       observer.disconnect();
     }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "divisor-experiencia") {
-      entry.target.classList.add("animated", "fadeInLeft");
-      observer.disconnect();
-    }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "divisor-clientes") {
-      entry.target.classList.add("animated", "fadeInDown", "delay-05s");
-      observer.disconnect();
-    }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "divisor-certificados") {
-      entry.target.classList.add("animated", "fadeInRight", "delay-1s");
-      observer.disconnect();
-    }
     else if (entry.isIntersecting && entry.target.dataset.animacion === "productos-pyme") {
       entry.target.classList.add("animated", "fadeIn");
       observer.disconnect();
@@ -38,18 +26,6 @@ const callback = (entries, observer) => {
     }
     else if (entry.isIntersecting && entry.target.dataset.animacion === "productos-vip") {
       entry.target.classList.add("animated", "fadeIn", "delay-1s");
-      observer.disconnect();
-    }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "productos-pyme2") {
-      entry.target.classList.add("animated", "fadeIn", "delay-1-5s");
-      observer.disconnect();
-    }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "productos-ph2") {
-      entry.target.classList.add("animated", "fadeIn", "delay-2s");
-      observer.disconnect();
-    }
-    else if (entry.isIntersecting && entry.target.dataset.animacion === "productos-vip2") {
-      entry.target.classList.add("animated", "fadeIn", "delay-2-5s");
       observer.disconnect();
     }
 
